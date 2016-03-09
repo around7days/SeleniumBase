2855d6afb9ae2d4c5c5d43f508aa959aafff8c5b
7days <7days@10.254.21.156>
2016-03-09 09:56:14 +0900

    commit

---
+++ src/test/java/selenium/com/AbstractSelenium.java
@@ -0,0 +1,97 @@
+package selenium.com;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import selenium.com.WebDriverFactory.Browser;
+
+/**
+ * Selenium抽象クラス
+ * @author 7days
+ */
+public abstract class AbstractSelenium {
+
+    /** ロガー */
+    @SuppressWarnings("unused")
+    private static final Logger logger = LoggerFactory.getLogger(AbstractSelenium.class);
+
+    /** プロパティ */
+    protected static final SeleniumPropertyManager prop = SeleniumPropertyManager.INSTANCE;
+
+    /** 実行ブラウザ */
+    protected static final Browser browser = Browser.getEnum(prop.getString("execute.browser"));
+
+    /** WebDriver */
+    protected static WebDriver driver = null;
+
+    /**
+     * エレメント取得
+     * @param by
+     * @return WebElement
+     */
+    protected WebElement $(By by) {
+        return driver.findElement(by);
+    }
+
+    /**
+     * エレメント取得(button)<br>
+     * input type="button" value="文字列"
+     * @param value 文字列
+     * @return WebElement
+     */
+    protected WebElement $buttonVal(String value) {
+        return driver.findElement(By.cssSelector("input[type='button'][value='" + value + "']"));
+    }
+
+    /**
+     * エレメント取得(selectBox)
+     * @param by
+     * @return Select
+     */
+    protected Select $select(By by) {
+        return new Select(driver.findElement(by));
+    }
+
+    /**
+     * エレメント取得(alert)
+     * @return Alert
+     */
+    protected Alert $alert() {
+        return driver.switchTo().alert();
+    }
+
+    /**
+     * WebDriverWaitの生成<br>
+     * （明示的な待機）
+     * @return WebDriverWait
+     */
+    protected WebDriverWait $wait() {
+        String waitDefault = prop.getString("wait.explicit.default");
+        return $wait(Integer.valueOf(waitDefault));
+    }
+
+    /**
+     * WebDriverWaitの生成<br>
+     * （明示的な待機）
+     * @param seconds 待機秒
+     * @return WebDriverWait
+     */
+    protected WebDriverWait $wait(int seconds) {
+        return new WebDriverWait(driver, seconds);
+    }
+
+    /**
+     * Sleep処理
+     * @param second 秒
+     * @throws InterruptedException
+     */
+    protected void sleep(int second) throws InterruptedException {
+        Thread.sleep(second * 1000);
+    }
+}
--- src/test/java/selenium/com/AbstractSeleniumTest.java
+++ src/test/java/selenium/com/AbstractSeleniumTest.java
@@ -4,7 +4,6 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,45 +11,32 @@ import org.slf4j.LoggerFactory;
  * SeleniumTest抽象クラス
  * @author 7days
  */
-public abstract class AbstractSeleniumTest {
+public abstract class AbstractSeleniumTest extends AbstractSelenium {
 
     /** ロガー */
     private static final Logger logger = LoggerFactory.getLogger(AbstractSeleniumTest.class);
 
-    /** プロパティ */
-    protected static final SeleniumPropertyManager prop = SeleniumPropertyManager.INSTANCE;
-
-    /** WebDriver */
-    protected static final WebDriver driver = WebDriverManager.INSTANCE.getDriver();
-
     @BeforeClass
     public static void init() {
-        logger.info("---------- 処理開始(class) ----------");
+        logger.debug("-------------------- 処理開始 --------------------");
+        logger.debug("open web browser : {}", browser.name());
+        /* Webブラウザの起動 */
+        driver = WebDriverFactory.create(browser);
     }
 
     @Before
     public void before() throws Exception {
-        logger.info("---------- 処理開始(test) ----------");
     }
 
     @After
     public void after() throws Exception {
-        logger.info("---------- 処理終了(test) ----------");
     }
 
     @AfterClass
     public static void finish() {
-        logger.info("---------- 処理終了(class) ----------");
+        logger.debug("close web browser : {}", browser.name());
         /* Webブラウザの終了 */
         driver.quit();
-    }
-
-    /**
-     * Sleep処理
-     * @param second 秒
-     * @throws InterruptedException
-     */
-    protected void sleep(int second) throws InterruptedException {
-        Thread.sleep(second * 1000);
+        logger.debug("-------------------- 処理終了 --------------------");
     }
 }
